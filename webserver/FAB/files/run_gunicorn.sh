84e0f65149e25e0615b606b2e85515ef5b3d24f3
Eran Keydar <ekeydar@gmail.com>
2014-03-12 12:27:09 +0200

    fixed FAB for opentrain.hasadna and new repo

--- webserver/FAB/files/run_gunicorn.sh
+++ webserver/FAB/files/run_gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd %(HOME)s/work/OpenTrains/webserver/opentrain
+cd %(HOME)s/work/OpenTrain/webserver/opentrain
 exec gunicorn -p %(HOME)s/opentrain.id \
     -b 127.0.0.1:9000 \
     -w 3 opentrain.wsgi:application
