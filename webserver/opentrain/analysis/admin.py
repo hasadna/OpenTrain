f7803f392cfbff66d3d87649e4a5337916b18630
Eran Keydar <ekeydar@gmail.com>
2014-04-07 22:15:34 +0300

    admin for trip ids

--- webserver/opentrain/analysis/admin.py
+++ webserver/opentrain/analysis/admin.py
@@ -7,8 +7,23 @@ class ReportAdmin(admin.ModelAdmin):
     
 admin.site.register(models.Report,ReportAdmin)
 
+class NonEmptyTripFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = ('trip')
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'trip'
+
+    def lookups(self, request, model_admin):
+        return models.RealTimeStop.objects.values('trip_id').distinct()
+
+    def queryset(self, request, queryset):
+        return queryset.filter(trip_id=self.value())
+
+
 class RealTimeStopAdmin(admin.ModelAdmin):
-    list_filter = ('trip',)
+    list_filter = (NonEmptyTripFilter,)
     
 admin.site.register(models.RealTimeStop,RealTimeStopAdmin)
 
