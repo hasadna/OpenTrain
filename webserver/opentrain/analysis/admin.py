fca33d75855a2afaa32112a1b317ff6fdef2e13b
Eran Keydar <ekeydar@gmail.com>
2014-04-07 22:01:35 +0300

    exp to unicode

--- webserver/opentrain/analysis/admin.py
+++ webserver/opentrain/analysis/admin.py
@@ -6,5 +6,12 @@ class ReportAdmin(admin.ModelAdmin):
     list_filter = ('device_id',)
     
 admin.site.register(models.Report,ReportAdmin)
+
+class RealTimeStopAdmin(admin.ModelAdmin):
+    list_filter = ('trip')
+    
+admin.site.register(models.RealTimeStop,RealTimeStopAdmin)
+
 common.ot_utils.autoregister('analysis')
+
 
