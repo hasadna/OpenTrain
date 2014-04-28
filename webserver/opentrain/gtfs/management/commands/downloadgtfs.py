bd68827ea4d56ef948d1e8c8eff5d5f2aa915a86
Eran Keydar <ekeydar@gmail.com>
2014-04-28 19:34:25 +0300

    add downloadgtfs command

---
+++ webserver/opentrain/gtfs/management/commands/downloadgtfs.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+import gtfs.utils
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Download gtfs file from MOT'
+    option_list = BaseCommand.option_list + (
+            make_option('--todb',
+            action='store_true',
+            dest='todb',
+            default=False,
+            help='Store also to db'),
+        )
+    def handle(self, *args, **options):
+        download_only = not options['todb']
+        self.stdout.write('Starting download_gtfs_file(donwload_only=%s)' % (download_only))
+        gtfs.utils.download_gtfs_file(download_only=download_only)
+        self.stdout.write('GTFS was donwloaded successfully')
+        
+
+                          
+                          
