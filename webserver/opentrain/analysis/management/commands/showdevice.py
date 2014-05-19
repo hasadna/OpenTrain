713c028706337dce6839f409c6a8ae97e7ed0958
Eran Keydar <ekeydar@gmail.com>
2014-05-19 20:06:07 +0300

    manage

--- webserver/opentrain/analysis/management/commands/showdevice.py
+++ webserver/opentrain/analysis/management/commands/showdevice.py
@@ -3,8 +3,19 @@ from optparse import make_option
 import analysis.utils
 
 class Command(BaseCommand):
-    args = '<device-id-partial-name>'
-    help = 'Analyze gps location of given report, can be given partially'
+    option_list = BaseCommand.option_list + ( 
+         make_option('--gps',
+            action='store_true',
+            dest='gps',
+            default=False,
+            help='Show gps info'),
+        make_option('--wifi',
+            action='store_true',
+            dest='wifi',
+            default=False,
+            help='Show wifi info'))
+        
+    help = 'Show device info'
     def handle(self, *args, **options):
         if len(args) != 1:
             raise CommandError("Must give device-id-partial-name")
@@ -15,13 +26,21 @@ class Command(BaseCommand):
             raise CommandError('Did not found any device id matching %s' % (device_pat))
         if len(device_ids) > 1:
             raise CommandError('Found more than one device id matching %s:\n%s' % (device_pat,'\n'.join(device_ids)))
-        device_id = device_ids[0]
-        msg = 'GPS REPORT for device %s' % device_id
+        self.device_id = device_ids[0]
+        if options['gps']:
+            self.print_gps_info()
+        if options['wifi']:
+            self.print_wifi_info()
+    
+    def print_title(self,msg):
         self.stdout.write('=' * (len(msg)))
         self.stdout.write(msg)
         self.stdout.write('=' * (len(msg)))
         
-        reports = analysis.utils.get_reports(device_id)
+    def print_gps_info(self):
+        self.print_title('GPS REPORT for device %s' % self.device_id)
+        
+        reports = analysis.utils.get_reports(self.device_id)
         reports_with_loc = [r for r in reports if hasattr(r,'my_loc')]
         r = None
         self.stdout.write('# of reports = %s' % (len(reports)))
@@ -39,8 +58,17 @@ class Command(BaseCommand):
             ts_diff = (r_cur.timestamp - r_cur.my_loc.timestamp).total_seconds()
             if dist_prev > threshold:
                 self.stdout.write(pat % (idx,r_cur.id,dist_prev,dist_next,r_cur.my_loc.accuracy,ts_diff))
-                
-                
+        
+    def print_wifi_info(self):
+        self.print_title('WIFI analysis for device %s' % self.device_id)
+        reports = analysis.utils.get_reports(self.device_id)
+        stations = []
+        for report in reports:
+            if report.is_station():
+                stations.append(report)
+        print 'Reports : %s' % len(reports)
+        print 'Reports in station: %s' % len(stations)
+            
                 
                           
                           
