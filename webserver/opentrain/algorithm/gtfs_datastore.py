d15f8a8f89e07d5402941e0b13060088265efac4
Ofer Bartal <ofer@bartals.com>
2014-05-26 22:28:17 +0300

    loading all gtfs days to redis

--- webserver/opentrain/algorithm/gtfs_datastore.py
+++ webserver/opentrain/algorithm/gtfs_datastore.py
@@ -174,12 +174,12 @@ def daterange(start_date, end_date):
     yield start_date + datetime.timedelta(n)
 
 def ReloadRedisGTFSData():
-  #days = gtfs.models.Service.objects.all().values_list('start_date', flat=True)
-  #days = sorted(list(set(days)))
-  days_begin_end = [_GTFSDayStrToDay('010414'), datetime.date.today()+datetime.timedelta(1)] # add 
-  days = daterange(days_begin_end[0], days_begin_end[1])
+  days = gtfs.models.Service.objects.all().values_list('start_date', flat=True)
+  days = sorted(list(set(days)))
+  #days_begin_end = [_GTFSDayStrToDay('010414'), datetime.date.today()+datetime.timedelta(1)]
+  #days = daterange(days_begin_end[0], days_begin_end[1])
   #days = days_begin_end
-  days = sorted([x for x in days])
+  #days = sorted([x for x in days])
   #days = [days[0]]
   for i, day in enumerate(days):
     print i, ' of ', len(days)
