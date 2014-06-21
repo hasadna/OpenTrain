c73ba05dc001de47514a21c7df74c6a2d631845d
Eran Keydar <ekeydar@gmail.com>
2014-06-21 22:55:20 +0300

    timetable

--- webserver/opentrain/timetable/models.py
+++ webserver/opentrain/timetable/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 class Stop(models.Model):
-    name = models.CharField(max_length=100)
     stop_id = models.IntegerField(db_index=True)
     stop_name = models.CharField(max_length=200)
     stop_lat = models.FloatField()
     stop_lon = models.FloatField()
-    stop_url = models.URLField()
-    
+    stop_url = models.URLField(blank=True,null=True)
+        
+    def __unicode__(self):
+        return '%s %s' % (self.stop_name,self.stop_id)
 
         
 
--- webserver/opentrain/timetable/utils.py
+++ webserver/opentrain/timetable/utils.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+import os.path
+from models import Stop
+
+DIR = os.path.join(settings.BASE_DIR,'tmp_data/gtfs/zip_data/2014_06_21_18_58_46')
 import csv
 def read_csv(filename):
     with open(filename) as fh:
@@ -14,6 +19,18 @@ def read_csv(filename):
                 
         
 def build_stops():
-    stops = read_csv('stops.txt')
-    
+    filename = os.path.join(DIR,'stops.txt')
+    stop_dicts = read_csv(filename)
+    for stop_dict in stop_dicts:
+        try:
+            existing_stop = Stop.objects.get(stop_id=stop_dict['stop_id'])  # @UnusedVariable
+        except Stop.DoesNotExist:
+            new_stop = Stop(stop_name=stop_dict['stop_name'],
+                            stop_id=stop_dict['stop_id'],
+                            stop_lat=stop_dict['stop_lat'],
+                            stop_lon=stop_dict['stop_lon'],
+                            stop_url=stop_dict['stop_url'])
+            new_stop.save()
+            print 'New stop created %s' % (new_stop)
+            
     
