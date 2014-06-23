6f28ac570a39b3d49c11f918981f1522829b065d
Eran Keydar <ekeydar@gmail.com>
2014-06-23 20:04:11 +0300

    tt

--- webserver/opentrain/timetable/models.py
+++ webserver/opentrain/timetable/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Stop(models.Model):
+class TtStop(models.Model):
     stop_id = models.IntegerField(db_index=True)
     stop_name = models.CharField(max_length=200)
     stop_lat = models.FloatField()
@@ -11,14 +11,14 @@ class Stop(models.Model):
         return '%s %s' % (self.stop_name,self.stop_id)
 
 
-class Trip(models.Model):
+class TtTrip(models.Model):
     trip_id = models.CharField(max_length=100,unique=True)
     shape_id = models.CharField(max_length=100)
         
-class StopTime(models.Model):
-    stop = models.ForeignKey('Stop')
+class TtStopTime(models.Model):
+    stop = models.ForeignKey(TtStop)
     stop_sequence = models.IntegerField()
     exp_arrival = models.DateTimeField()
     exp_departure = models.DateTimeField()
-    trip = models.ForeignKey('Trip')
+    trip = models.ForeignKey(TtTrip)
     
